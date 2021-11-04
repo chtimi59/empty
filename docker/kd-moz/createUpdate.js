f56e16ef8ece365b22867f9e6b9d48e02f1c74f4
Jan Dorgeville <jdorgeville@gmail.com>
2021-11-04 23:57:59 +0100

    test

--- docker/kd-moz/createUpdate.js
+++ docker/kd-moz/createUpdate.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
 
-const base = 'docker/kd-moz/www/'
-
-async function main(ctx) {
-    // ({github, context, core}) => ctx
-    const manifest = JSON.parse(fs.readFileSync(base + 'manifest.json', 'utf8').toString());
-    console.log(manifest.version)
-    console.log("hi there!")
+async function main(base_url, extension_id) {
+    const manifest = JSON.parse(fs.readFileSync('docker/kd-moz/www/manifest.json', 'utf8').toString());
+    const store = { "addons": { } }
+    store.addons[extension_id] = {
+        "updates": [
+            {
+              "version": manifest.version,
+              "update_link": `${base_url}/firefox_extension.xpi`
+            }
+        ]
+    }
+    fs.writeFileSync('docker/kd-moz/www/update.json', JSON.stringify(store, null, 4))
 }
 
 module.exports = main 
-//main()
+//main("https://","{1234}")
