5133131a1ceabb0999afee896fd970bfe9b4b644
Jan Dorgeville <jdorgeville@gmail.com>
2021-11-04 12:30:26 +0100

    rr

--- docker/kd-moz/createUpdate.js
+++ docker/kd-moz/createUpdate.js
@@ -1,3 +1,13 @@
-module.exports = async ({github, context, core}) => {
-    console.log("hi there!")    
+const fs = require('fs')
+
+const base = 'docker/kd-moz/www/'
+
+async function main(ctx) {
+    // ({github, context, core}) => ctx
+    const manifest = JSON.parse(fs.readFileSync(base + 'manifest.json', 'utf8').toString());
+    console.log(manifest.version)
+    console.log("hi there!")
 }
+
+module.exports = main 
+//main()
